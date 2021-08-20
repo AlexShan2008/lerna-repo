8e514712f036788a515571279bd2776e1a1dda7e
Alex Shan <shantong.sir@gmail.com>
2021-08-20 23:28:13 +0800

    feat(*): add TypeScript support

--- packages/date-helpers/__tests__/date-helpers.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const dateHelpers = require('..');
-
-describe('date-helpers', () => {
-    it('needs tests');
-});
---
+++ packages/date-helpers/__tests__/date-helpers.test.ts
@@ -0,0 +1,7 @@
+import { DateHelpers } from '../src/date-helpers';
+
+describe('date-helpers', () => {
+  it('formatMoney: 1000000 with dollors', () => {
+    expect(DateHelpers.ToString()).toBe('date');
+  });
+});
