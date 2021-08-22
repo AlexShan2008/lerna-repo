f8a319e9d023e32e3c830d6d400747057d3f4c3e
Alex Shan <shantong.sir@gmail.com>
2021-08-22 19:10:30 +0800

    feat(object-helpers): update the unit test case

--- packages/object-helpers/__tests__/object-helpers.test.ts
+++ packages/object-helpers/__tests__/object-helpers.test.ts
@@ -1,7 +1,7 @@
 import { ObjectHelpers } from '../src/object-helpers';
 
 describe('object-helpers', () => {
-  it('formatMoney: 1000000 with dollors', () => {
+  it("getKeys: { a: 1, b: '2' } toStrictEqual ['a', 'b']", () => {
     expect(ObjectHelpers.getKeys({ a: 1, b: '2' })).toStrictEqual(['a', 'b']);
   });
 });
